96659f7d081a4a5270e149257705c350a909becd
unknown <paatatepnadze1@gmail.com>
2022-11-21 13:59:02 +0100

    create input styles

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -4,9 +4,17 @@ import Wrapper from "./components/Wrapper";
 function App() {
   return (
     <div className="App">
-      <Wrapper>
+      <Wrapper color="black">
         <h2>somthing inside wrapper</h2>
         <button>Click me!</button>
+      </Wrapper>
+      <Wrapper color="red">
+        <h2>Another wrapper</h2>
+        <button>some description</button>
+        <input
+          type="text"
+          placeholder="Enter value"
+        />
       </Wrapper>
     </div>
   );
--- 19-children-prop/src/components/Wrapper.js
+++ 19-children-prop/src/components/Wrapper.js
@@ -1,10 +1,17 @@
 function Wrapper(props) {
   console.log(props);
+
+  const style = {
+    backgroundColor: props.color,
+    width: "550px",
+    padding: "20px auto",
+    margin: "20px auto",
+  };
   return (
-    <>
+    <div style={style}>
       <h1>Wrapper Works!</h1>
       {props.children}
-    </>
+    </div>
   );
 }
 
