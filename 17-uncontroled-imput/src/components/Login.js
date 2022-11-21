a6b55c448fb6e6ff9d7a69bb6869ce2e66c9bfea
unknown <paatatepnadze1@gmail.com>
2022-11-21 11:31:29 +0100

    create login form /inputs

---
+++ 17-uncontroled-imput/src/components/Login.js
@@ -0,0 +1,38 @@
+function Login() {
+  function handelFormSubmit(event) {
+    //prevent page relodding
+    event.preventDefault();
+    const userData = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    console.log(userData);
+    alert(JSON.stringify(userData));
+  }
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handelFormSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
