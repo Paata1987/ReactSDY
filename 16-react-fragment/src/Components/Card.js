548b64f8c893f15a11b48ece629dc32a92ef3d3b
unknown <paatatepnadze1@gmail.com>
2022-11-21 10:28:00 +0100

    create react/fragment element

---
+++ 16-react-fragment/src/Components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <h1>Paata</h1>
+      <h3>::::::::::: Something :::::::::::: </h3>
+      <button>Like!</button>
+    </>
+  );
+};
+
+export default Card;
