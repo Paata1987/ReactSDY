80b24ee31ac525503c2beeb23444b2c558fb1138
unknown <paatatepnadze1@gmail.com>
2022-11-22 14:26:22 +0100

    add different way/Async await/IIFE est

--- 22-useEffect-async-await/src/components/Posts.js
+++ 22-useEffect-async-await/src/components/Posts.js
@@ -6,7 +6,54 @@ function Posts() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const API_URL = "https://jsonplaceholder.typicode.com/posts";
+
   useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL);
+        const post = await res.json();
+        setPosts(post);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+  /*
+
+  //IIFE => imidiatly invoke function expression
+  useEffect(() => {
+    (async function() {
+      try {
+        const res = await fetch(API_URL);
+        const post = await res.json();
+        setPosts(post);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    })()
+    
+  }, []);
+  
+  */
+
+  /*  useEffect(async () => {
+    try {
+      const res = await fetch(API_URL);
+      const post = await res.json();
+      setPosts(post);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []); */
+
+  /* useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
       .then((posts) => {
@@ -16,7 +63,8 @@ function Posts() {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, []); */
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.
 
   if (error) {
     return <h1>Error: {error}</h1>;
