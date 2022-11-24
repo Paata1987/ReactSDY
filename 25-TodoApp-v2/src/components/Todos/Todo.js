1e0f5ca5aca12bf9aa722e8d9b8a32661177e1b1
unknown <paatatepnadze1@gmail.com>
2022-11-24 09:52:39 +0100

    add button input fields styles/icon styles

--- 25-TodoApp-v2/src/components/Todos/Todo.js
+++ 25-TodoApp-v2/src/components/Todos/Todo.js
@@ -1,4 +1,5 @@
-import { RiTodoFill } from "react-icons/ri";
+import { RiDeleteBin2Line, RiTodoFill } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa";
 import styles from "./Todo.module.css";
 function Todo(props) {
   const { todo, deleteTodo } = props;
@@ -9,6 +10,8 @@ function Todo(props) {
     >
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}> {todo.text} </div>
+      <RiDeleteBin2Line className={styles.deleteIcon} />
+      <FaCheck className={styles.checkIcon} />
     </div>
   );
 }
