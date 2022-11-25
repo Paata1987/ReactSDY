23d2c9e30aef826a3d488f16ce997a88eb82d42f
unknown <paatatepnadze1@gmail.com>
2022-11-25 15:11:50 +0100

    create omponetn routing  kay Navigate elements

--- 26-reactRouter/src/components/Courses.jsx
+++ 26-reactRouter/src/components/Courses.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import courses from "../data/Courses";
+import queryString from "query-string";
+
+const SORT_KEY = ["title", "slug", "id"];
+
+function sortCourses(courses, key) {
+  const sortedCourses = [...courses];
+  if (!key || !SORT_KEY.includes(key)) {
+    return sortedCourses;
+  }
+  sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+  return sortedCourses;
+}
+
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const navigate = useNavigate();
+  const [sortKey] = useState(query.sort);
+  // console.log(sortCourses(courses, sortKey));
+  const [sortedCourses] = useState(sortCourses(courses, sortKey));
+
+  useEffect(() => {
+    if (!SORT_KEY.includes(sortKey)) {
+      navigate(".");
+    }
+  }, [sortKey, navigate]);
+
   return (
     <div>
-      <h1>Courses</h1>
-      {courses.map((course) => (
+      <h1>{sortKey ? `Courses sortet by ${sortKey}` : "Courses"} </h1>
+      {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link
             className="courseLink"
--- 26-reactRouter/src/components/SingleCourse.jsx
+++ 26-reactRouter/src/components/SingleCourse.jsx
@@ -1,18 +1,30 @@
-import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/Courses";
-import NotFound from "./NotFound";
+
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
+
   const course = courses.find((course) => course.slug === params.courseSlug);
-  if (!course) {
-    return <NotFound />;
-  }
+
+  //routing navigate
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course, navigate]);
+
+  ///simply show not found
+  /*  if (!course) {
+    return <NotFound />; 
+  } */
 
   return (
     <div>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
 
       <Link
         to=".."
