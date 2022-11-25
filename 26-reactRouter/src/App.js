49e9b35b7523fb439d0a329ceba6489792a04635
unknown <paatatepnadze1@gmail.com>
2022-11-25 09:22:09 +0100

    add homepage rout /

--- 26-reactRouter/src/App.js
+++ 26-reactRouter/src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>ReactRouter</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={<h1>Home</h1>}
+          />
+        </Routes>
       </div>
     </BrowserRouter>
   );
